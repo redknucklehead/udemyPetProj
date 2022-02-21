76add5a9de1a6ad3989c6945f30abc01434c2eaa
DmitryVorobyev <radredknucklehead@gmail.com>
2022-02-21 20:44:05 +0300

    loopAndCondition

--- script.js
+++ script.js
@@ -7,16 +7,30 @@ const numberOfFilms = +prompt('Сколько фильмов вы уже пос�
     actors: {},
     genres: [],
     privat: false
-    },
-      a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = +prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = +prompt('На сколько оцените его?', '');
+    };
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = +prompt('На сколько оцените его?', '');
+ 
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
 
+
+if (personalMovieDB.count < 10) {
+  alert('Просмотрено слишком мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  alert('Вы киноман');
+}else {
+  alert('Произошла ошибка');
+}
 
 
 console.log(personalMovieDB);
